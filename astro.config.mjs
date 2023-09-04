72601293af2d588cbf21be3f0db1eba53f3e7c96
jonathanmv <jonathanmv@outlook.com>
2023-09-04 20:02:00 +0200

    add deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://jonathanmv.github.io",
+  // @TODO Remove after setting custom domain
+  base: "/patoland-blog",
+  integrations: [mdx(), sitemap()],
 });
